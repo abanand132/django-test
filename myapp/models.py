0cf8cda505f8f8d9d7eb2f203630f56550c55635
Abhishek Anand <abanand132@gmail.com>
2024-11-10 19:54:52 +0530

    Revert "change"
    This reverts commit 75b520f7dd561cbfe74a39fdf1ce79e98d9a4ba2.

--- myapp/models.py
+++ myapp/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profiles')
     name = models.CharField(max_length=100)
-    profile = models.ImageField(upload_to='profile', default='profile/default.webp')
+    profile = models.ImageField(upload_to='profile')
 
     def __str__(self):
         return self.name
